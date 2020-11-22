d9c37c4ca98be38c125e17214fedaa1abdd30f34
nabilahrisa20 <74880200+nabilahrisa20@users.noreply.github.com>
2020-11-23 06:29:02 +0700

    Add files via upload

---
+++ konfigurasi.java
@@ -0,0 +1,27 @@
+package com.nabilah.pegawaiapp;
+
+public class konfigurasi {
+
+    public static final String URL_ADD="http://192.168.1.10/Android/pegawai/tambahPgw.php";
+    public static final String URL_GET_ALL = "http://192.168.1.10/Android/pegawai/tampilSemuaPgw.php";
+    public static final String URL_GET_EMP = "http://192.168.1.10/Android/pegawai/tampilPgw.php?id=";
+    public static final String URL_UPDATE_EMP = "http://192.168.1.10/Android/pegawai/updatePgw.php";
+    public static final String URL_DELETE_EMP = "http://192.168.1.10/Android/pegawai/hapusPgw.php?id=";
+
+    //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Skrip PHP
+    public static final String KEY_EMP_ID = "id";
+    public static final String KEY_EMP_NAMA = "name";
+    public static final String KEY_EMP_POSISI = "desg"; //desg itu variabel untuk posisi
+    public static final String KEY_EMP_GAJI = "salary"; //salary itu variabel untuk gaji
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "name";
+    public static final String TAG_POSISI = "desg";
+    public static final String TAG_GAJI = "salary";
+
+    //ID karyawan
+    //emp itu singkatan dari Employee
+    public static final String EMP_ID = "emp_id";
+}
