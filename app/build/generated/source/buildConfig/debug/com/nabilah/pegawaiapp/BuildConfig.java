f58c0437dd878dc656608973461b8ddfa92f785e
nabilahrisa20 <nabilahrisa20@gmail.com>
2020-11-23 05:52:21 +0700

    third commit

---
+++ app/build/generated/source/buildConfig/debug/com/nabilah/pegawaiapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nabilah.pegawaiapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.nabilah.pegawaiapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/com/nabilah/pegawaiapp/ListView.java
@@ -0,0 +1,14 @@
+package com.nabilah.pegawaiapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+    }
+}
